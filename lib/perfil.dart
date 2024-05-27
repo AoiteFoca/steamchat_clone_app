c2392ae78de8cf37c6404abd3339da668b6d94fd
AoiteFoca <nathancielusinski@gmail.com>
2024-05-27 00:51:48 -0300

    Adição INICIAL do crud ao perfil
    Função de delete funcional;
    alteração de foto (por url);
    alteração do nickname;
    Envio do status para o firebase;
    Exemplos limitados à alteração manual de id no código - Será arrumado posteriormente;
    Adição de novos botões na tela de perfil (Config e delete);
    Confirmação de logout e delet da conta adicional aplicada.

--- lib/perfil.dart
+++ lib/perfil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'addamigos.dart';
 
 class PerfilPage extends StatefulWidget {
@@ -8,6 +9,59 @@ class PerfilPage extends StatefulWidget {
 
 class _PerfilPageState extends State<PerfilPage> {
   String _status = 'Online'; // Define o status padrão
+  String _nickname = ' ';
+  String _photoUrl = 'https://avatars.cloudflare.steamstatic.com/8d1eebb8364ac6ecc76bd9d3e13ff8e9743816fd_full.jpg';
+  final TextEditingController _nicknameController = TextEditingController();
+  final TextEditingController _photoUrlController = TextEditingController();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final String _userId = '4701'; // ID fictício do usuário
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProfile();
+  }
+
+  Future<void> _loadUserProfile() async {
+    DocumentSnapshot userDoc = await _firestore.collection('users').doc(_userId).get();
+    if (userDoc.exists) {
+      setState(() {
+        _nickname = userDoc['nickname'] ?? 'Nickname';
+        _photoUrl = userDoc['photoUrl'] ?? _photoUrl;
+        _status = userDoc['status'] ?? 'Offline';
+        _nicknameController.text = _nickname;
+        _photoUrlController.text = _photoUrl;
+      });
+    }
+  }
+
+  Future<void> _updateStatus(String status) async {
+    await _firestore.collection('users').doc(_userId).update({'status': status});
+    setState(() {
+      _status = status;
+    });
+  }
+
+  Future<void> _updateProfile() async {
+    await _firestore.collection('users').doc(_userId).update({
+      'nickname': _nicknameController.text,
+      'photoUrl': _photoUrlController.text,
+    });
+    setState(() {
+      _nickname = _nicknameController.text;
+      _photoUrl = _photoUrlController.text;
+    });
+    Navigator.pop(context); // Fechar o modal após a atualização
+  }
+
+  Future<void> _deleteAccount() async {
+    await _firestore.collection('users').doc(_userId).delete();
+  }
+
+  Future<void> _logout() async {
+    Navigator.popUntil(context, ModalRoute.withName('login'));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,49 +96,46 @@ class _PerfilPageState extends State<PerfilPage> {
                                   decoration: BoxDecoration(
                                     shape: BoxShape.circle,
                                     border: Border.all(
-                                        color: _getBorderColor(),
-                                        width:
-                                            4.0), // Personaliza a cor e a largura da borda conforme necessário
+                                      color: _getBorderColor(),
+                                      width: 4.0, // Personaliza a cor e a largura da borda conforme necessário
+                                    ),
                                   ),
                                   child: CircleAvatar(
                                     radius: constraints.maxWidth * 0.15,
                                     // Define o raio do avatar como 15% da largura total
-                                    backgroundImage: NetworkImage(
-                                      'https://avatars.cloudflare.steamstatic.com/8d1eebb8364ac6ecc76bd9d3e13ff8e9743816fd_full.jpg',
-                                    ),
-                                    //Imagem de perfil
+                                    backgroundImage: NetworkImage(_photoUrl),
                                     backgroundColor: Colors.transparent,
                                     foregroundColor: _getBorderColor(),
                                     child: Text(''),
                                   ),
                                 ),
-                                SizedBox(height: 16.0),// Espaçamento
-                                Text(
-                                  'Teste Nome',
-                                  style: TextStyle(
-                                      fontSize: constraints.maxWidth * 0.05,
-                                      fontWeight: FontWeight.bold),
+                                SizedBox(height: 16.0), // Espaçamento
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      _nickname,
+                                      style: TextStyle(
+                                          fontSize: constraints.maxWidth * 0.05,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
                                 ),
                                 SizedBox(height: constraints.maxWidth * 0.1), // Espaçamento
                                 Text(
                                   'Status',
-                                  style: TextStyle(
-                                      fontSize: constraints.maxWidth * 0.050),
+                                  style: TextStyle(fontSize: constraints.maxWidth * 0.050),
                                 ),
                                 SizedBox(height: constraints.maxWidth * 0.01),
                                 // Botões de alternância para selecionar o status
                                 OutlinedButton(
                                   onPressed: () {
-                                    setState(() {
-                                      _status = 'Online';
-                                    });
+                                    _updateStatus('Online');
                                   },
                                   child: Text('Online'),
                                   style: ButtonStyle(
-                                    // Estiliza o botão conforme o status atual
-                                    side: MaterialStateProperty.resolveWith<
-                                        BorderSide>(
-                                      (Set<MaterialState> states) {
+                                    side: MaterialStateProperty.resolveWith<BorderSide>(
+                                          (Set<MaterialState> states) {
                                         if (_status == 'Online') {
                                           return BorderSide(
                                             color: Colors.blue,
@@ -101,16 +152,12 @@ class _PerfilPageState extends State<PerfilPage> {
                                 SizedBox(height: constraints.maxWidth * 0.01),
                                 OutlinedButton(
                                   onPressed: () {
-                                    setState(() {
-                                      _status = 'Ocupado';
-                                    });
+                                    _updateStatus('Ocupado');
                                   },
                                   child: Text('Ocupado'),
                                   style: ButtonStyle(
-                                    // Estiliza o botão conforme o status atual
-                                    side: MaterialStateProperty.resolveWith<
-                                        BorderSide>(
-                                      (Set<MaterialState> states) {
+                                    side: MaterialStateProperty.resolveWith<BorderSide>(
+                                          (Set<MaterialState> states) {
                                         if (_status == 'Ocupado') {
                                           return BorderSide(
                                             color: Colors.blue,
@@ -127,16 +174,12 @@ class _PerfilPageState extends State<PerfilPage> {
                                 SizedBox(height: constraints.maxWidth * 0.01),
                                 OutlinedButton(
                                   onPressed: () {
-                                    setState(() {
-                                      _status = 'Invisível';
-                                    });
+                                    _updateStatus('Invisível');
                                   },
                                   child: Text('Invisível'),
                                   style: ButtonStyle(
-                                    // Estiliza o botão conforme o status atual
-                                    side: MaterialStateProperty.resolveWith<
-                                        BorderSide>(
-                                      (Set<MaterialState> states) {
+                                    side: MaterialStateProperty.resolveWith<BorderSide>(
+                                          (Set<MaterialState> states) {
                                         if (_status == 'Invisível') {
                                           return BorderSide(
                                             color: Colors.blue,
@@ -162,16 +205,29 @@ class _PerfilPageState extends State<PerfilPage> {
                         onTap: () {
                           Navigator.push(
                             context,
-                            MaterialPageRoute(
-                                builder: (context) => AddAmigosPage()),
+                            MaterialPageRoute(builder: (context) => AddAmigosPage()),
                           );
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.settings),
+                        title: Text('Configurar Conta'),
+                        onTap: () {
+                          _showEditProfileModal();
                         },
                       ),
                       ListTile(
                         leading: Icon(Icons.exit_to_app),
                         title: Text('Sair'),
                         onTap: () {
-                          Navigator.popUntil(context, ModalRoute.withName('login'));
+                          _showLogoutConfirmation();
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.delete_forever),
+                        title: Text('Apagar Conta'),
+                        onTap: () {
+                          _showDeleteAccountConfirmation();
                         },
                       ),
                     ],
@@ -203,5 +259,97 @@ class _PerfilPageState extends State<PerfilPage> {
       default:
         return Colors.transparent; // Padrão: sem borda para garantir
     }
+  }
+
+  void _showEditProfileModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Editar Perfil'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: _nicknameController,
+                  decoration: InputDecoration(labelText: 'Nickname'),
+                ),
+                TextField(
+                  controller: _photoUrlController,
+                  decoration: InputDecoration(labelText: 'Foto (URL)'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Salvar'),
+              onPressed: () {
+                _updateProfile();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showLogoutConfirmation() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmação de Saída'),
+          content: Text('Você realmente quer sair da sua conta?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Não'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Sim'),
+              onPressed: () {
+                _logout();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteAccountConfirmation() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmação de Exclusão'),
+          content: Text('Você realmente quer apagar sua conta? Esta ação não pode ser desfeita.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Não'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Sim'),
+              onPressed: () {
+                _deleteAccount();
+                Navigator.popUntil(context, ModalRoute.withName('login'));
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
